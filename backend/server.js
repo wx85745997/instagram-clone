7313f63e52214d8a67615e16c1722527bc0895b6
fei tang <497045233@qq.com>
2021-12-29 12:30:25 +0800

    prisma init
    Signed-off-by: fei tang <497045233@qq.com>

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server'
-
-const typeDefs = gql`
-    type Movie{
-        title: String
-        year:Int
-    }
-    type Query{
-        movies: [Movie]
-        movie : Movie
-    }
-    type Mutation{
-        createMovie(title:String!): Boolean
-        deleteMovie(title:String!): Boolean
-    }
-`
-
-const resolvers = {
-    Query: {
-        movies: () => [],
-        movie: () => ({ title: "hello", year: 2021 })
-    },
-    Mutation: {
-        createMovie: (_, args) => {
-            console.log(args)
-            return true
-        },
-        deleteMovie: (_, args) => {
-            console.log(args)
-            return true
-        }
-    }
-}
+import { typeDefs, resolvers } from './schema.js'
 
 const server = new ApolloServer({
     typeDefs,
